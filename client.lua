0a55ab40ba09ae585ba2017edc174d81285a1858
Madsen <76999316+imMadsen@users.noreply.github.com>
2022-08-20 15:43:15 +0200

    Added PlayerVariable
     PlayerVariable, these are only networked between the given player

--- client.lua
+++ client.lua
@@ -1,12 +1,24 @@
 Variables = {}
+PlayerVariables = {}
 
-exports("GetVariableValue", function(variable, fallback)
+function GetVariable(variable, fallback)
     return Variables[variable] or fallback
-end)
+end
+
+function GetPlayerVariable(variable, fallback)
+    return PlayerVariables[variable] or fallback
+end
 
 RegisterNetEvent("netvar:_Update", function(variable, value, oldValue)
     Variables[variable] = value
-    TriggerEvent("netvar:Update", variable, value, oldValue)
+end)
+
+RegisterNetEvent("netvar:_UpdatePlayer", function(variable, value, oldValue)
+    PlayerVariables[variable] = value
 end)
 
 TriggerServerEvent("netvar:Sync")
+
+exports("GetPlayerVariable", GetPlayerVariable)
+exports("GetVariable", GetVariable)
+exports("GetVariableValue", GetVariable) -- DEPRECATED (Only here for backwards capability)
--- server.lua
+++ server.lua
@@ -1,18 +1,53 @@
 Variables = {}
+PlayerVariables = {}
 
-exports("GetVariableValue", function(variable)
+-- Global
+function GetVariable(variable, fallback)
     return Variables[variable]
-end)
+end
 
-exports("SetVariableValue", function(variable, value)
+function SetVariable(variable, value)
     local oldValue = Variables[variable]
-    TriggerClientEvent("netvar:_Update", -1, variable, value, oldValue)
     Variables[variable] = value
-end)
 
+    TriggerClientEvent("netvar:_Update", -1, variable, value, oldValue)
+end
+
+-- Player specific
+function GetPlayerVariable(variable, fallback)
+    local guid = GetPlayerGuid(source)
+    local variableIndex = string.format("%s:%s", guid, variable)
+    return PlayerVariables[variableIndex]
+end
+
+function SetPlayerVariable(variable, source, value)
+    local guid = GetPlayerGuid(source)
+    
+    local variableIndex = string.format("%s:%s", guid, variable)
+    local oldValue = PlayerVariables[variableIndex]
+    PlayerVariables[variableIndex] = value
+
+    TriggerClientEvent("netvar:_UpdatePlayer", source, variable, value, oldValue)
+end
+
+-- Sync
 RegisterNetEvent("netvar:Sync", function()
     local src = source
+
     for variable, value in pairs(Variables) do
         TriggerClientEvent("netvar:_Update", src, variable)
     end
+
+    for variable, value in pairs(PlayerVariables) do
+        TriggerClientEvent("netvar:_UpdatePlayer", src, variable)
+    end
 end)
+
+-- Exports
+exports("SetPlayerVariable", SetPlayerVariable)
+exports("GetPlayerVariable", GetPlayerVariable)
+
+exports("SetVariable", SetVariable)
+exports("SetVariableValue", SetVariable) -- DEPRECATED (Only here for backwards capability)
+exports("GetVariable", GetVariable)
+exports("GetVariableValue", GetVariable) -- DEPRECATED (Only here for backwards capability)
